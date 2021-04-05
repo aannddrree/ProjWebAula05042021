d38576cbcfe3de4a7594fc471766bead8ef9838c
unknown <aannddrree>
2021-04-05 19:47:22 -0300

    atualizacao

---
+++ ProjWebAula05042021/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using Dal;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjWebAula05042021
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadGrid();
+        }
+
+        protected void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            Employee employee = getData();
+            var db = new EmployeeDB();
+
+            if (db.Insert(employee))
+            {
+                LblMsg.Text = "Registro inserido!";
+                LoadGrid();
+            }
+            else
+                LblMsg.Text = "Erro ao inserir registro";
+        }
+
+        private Employee getData()
+        {
+            return new Employee()
+            {
+                Name = TxtNome.Text,
+                Telephone = TxtTelefone.Text
+            };
+        }
+
+        private void LoadGrid()
+        {
+            GVEmployee.DataSource = new EmployeeDB().GetAll();
+            GVEmployee.DataBind();
+        }
+    }
+}
---
+++ ProjWebAula05042021/WebForm1.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ProjWebAula05042021
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle TxtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNome;
+
+        /// <summary>
+        /// Controle TxtTelefone.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtTelefone;
+
+        /// <summary>
+        /// Controle LblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMsg;
+
+        /// <summary>
+        /// Controle BtnSalvar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSalvar;
+
+        /// <summary>
+        /// Controle GVEmployee.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVEmployee;
+    }
+}
